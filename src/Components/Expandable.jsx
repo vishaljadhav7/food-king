6699ee7a483e4afd462974a691fce5efa6f8de85
vishaljadhav7 <vishalj.7661@gmail.com>
2024-08-20 14:35:47 +0530

    minor changes

--- src/Components/Expandable.jsx
+++ src/Components/Expandable.jsx
@@ -4,13 +4,17 @@ const Expandable = ({children}) => {
 
  const [expand, setExpand] = useState(false)   
     
- if(children.length <= 100) return <p>{children}</p>
+ if(children?.length <= 100) return <p>{children}</p>
 
- const text =  expand ?  children : children.substring(0,100) 
+ const text =  expand ?  children : children?.substring(0,100) 
+
+ const handleClick = () => {
+   setExpand(!expand)
+ }
 
  
   return (
-     <p>{text}   <span className='px-2 bg-black cursor-pointer rounded-sm text-white' onClick={()=>setExpand(!expand)}>{expand ? 'less' : 'more'}</span>  </p>   
+     <p>{text} <span className='px-2 bg-black cursor-pointer rounded-sm text-white' onClick={handleClick}>{expand ? 'less' : 'more'}</span>  </p>   
 
   )
 }
