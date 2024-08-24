dfd4aa4c6a833e833709fd1a982d6ca75f503478
vishaljadhav7 <vishalj.7661@gmail.com>
2024-08-24 16:55:17 +0530

    designed  a sign up form

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -5,24 +5,24 @@ const SignUp = () => {
      e.preventDefault()
   }
   return (
-    <div>
-          <form>
-              <div className=''>
+    <div className=' h-[87.3%] flex justify-center items-center'>
+          <form className='bg-fuchsia-400 md:w-[320px] w-full  h-[60%] flex flex-col justify-between p-4 shadow-2xl shadow-stone-500 rounded-lg'>
+              <div className='flex flex-col gap-1'>
                   <label htmlFor="name">Name</label>
-                  <input type="text" placeholder='your name' id="name"/>
+                  <input type="text" placeholder='your name' id="name" className='p-2'/>
               </div> 
 
-              <div>
+              <div className='flex flex-col gap-1'>
                  <label htmlFor="email">Email</label>
-                 <input type="text" placeholder='your email' id="email"/>
+                 <input type="text" placeholder='your email' id="email" className='p-2'/>
               </div>
  
-               <div>
-                 <label htmlFor="passcode">Email</label>
-                 <input type="password" placeholder='enter your password' id="passcode"/>
+               <div className='flex flex-col gap-1'>
+                 <label htmlFor="passcode">Password</label>
+                 <input type="password" placeholder='enter your password' id="passcode" className='p-2'/>
               </div>  
 
-              <button type='submit' onClick={handleSubmit}>Sign Up</button>
+              <button type='submit' onClick={handleSubmit} className='p-2 w-28 rounded-2xl bg-indigo-600 text-white hover:scale-110'>Sign Up</button>
           </form>      
     </div>
   )
