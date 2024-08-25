cc1043c128955b78037a91bf8c19cb93aeb26e6a
vishaljadhav7 <vishalj.7661@gmail.com>
2024-08-25 11:49:50 +0530

    made the cart more responsive

--- src/Components/CartItem.jsx
+++ src/Components/CartItem.jsx
@@ -1,66 +1,132 @@
 import React from 'react'
-import {useSelector, useDispatch} from 'react-redux'
+import {useDispatch} from 'react-redux'
 import {addItem, decrementItem, removeItem}  from '../features/cartSlice'
 import { FaTrash } from "react-icons/fa6";
 import { FaPlus } from "react-icons/fa6";
 import { FaMinus } from "react-icons/fa6";
 
 
-const CartItem = ({data}) => {
+// const CartItem = ({data}) => {
 
      
-    const dispatch = useDispatch()
-    const totalPrice = Math.floor((data.cartQuantity  *( data.defaultPrice || data.price))/100)
+//     const dispatch = useDispatch()
+//     const totalPrice = Math.floor((data.cartQuantity  *( data.defaultPrice || data.price))/100)
 
-     const handleRemoveItem = () => {
-        dispatch(removeItem(data.id))
-     }
+//      const handleRemoveItem = () => {
+//         dispatch(removeItem(data.id))
+//      }
 
-     const handleAddItem = () =>{
-        dispatch(addItem(data))
-     }
+//      const handleAddItem = () =>{
+//         dispatch(addItem(data))
+//      }
 
-     const handleDecreaseItem = ( ) =>{
-        dispatch(decrementItem(data))
-     }
+//      const handleDecreaseItem = ( ) =>{
+//         dispatch(decrementItem(data))
+//      }
 
-    return (
-           <div className= "flex h-32 justify-between border-b-2 border-purple-400 mb-5">
-               <div className='w-18 flex  mb-3 gap-4'>
-                  <img src={"https://media-assets.swiggy.com/swiggy/image/upload/"+ data.imageId} alt="" className='w-2/6 hidden md:block' /> 
-               <div className='w-[100px] '>
-                   <h3 className='font-semibold md:text-[13px] md:w-60'>{data.name}</h3>
-                    <button onClick={handleRemoveItem}><FaTrash /></button>
-               </div>
-             </div>
+//     return (
+//            <div className= "flex h-32 justify-between border-b-2 border-purple-400 mb-5">
+//                <div className='w-18 flex  mb-3 gap-4'>
+//                   <img src={"https://media-assets.swiggy.com/swiggy/image/upload/"+ data.imageId} alt="" className='w-2/6 hidden md:block' /> 
+//                <div className='w-[100px] '>
+//                    <h3 className='font-semibold md:text-[13px] md:w-60'>{data.name}</h3>
+//                     <button onClick={handleRemoveItem}><FaTrash /></button>
+//                </div>
+//              </div>
   
-              <div className='font-semibold text-1xl md:-ml-64 text-center'>
+//               <div className='font-semibold text-1xl md:-ml-64 text-center'>
                   
-                  <h3>{(data.defaultPrice || data.price)/100}/-</h3>
-              </div> 
+//                   <h3>{(data.defaultPrice || data.price)/100}/-</h3>
+//               </div> 
     
-                <div className='md:flex flex-col items-center justify-center text-[18px] md:-mt-22 text-center gap-3 ml-8 md:-ml-8'>
-                   <button 
-                   className='p-1 bg-teal-300 text-white font-semibold rounded-md'
-                    onClick={handleAddItem}
-                   >
-                    <FaPlus /> 
-                  </button>
+//                 <div className='md:flex flex-col items-center justify-center text-[18px] md:-mt-22 text-center gap-3 ml-8 md:-ml-8'>
+//                    <button 
+//                    className='p-1 bg-teal-300 text-white font-semibold rounded-md'
+//                     onClick={handleAddItem}
+//                    >
+//                     <FaPlus /> 
+//                   </button>
 
-                   <h3 className='font-semibold'>{data.cartQuantity}</h3>
+//                    <h3 className='font-semibold'>{data.cartQuantity}</h3>
                 
-                   <button 
-                   className='p-1 bg-teal-300 text-white font-semibold rounded-md'
-                    onClick={handleDecreaseItem}
-                   >
-                    <FaMinus /></button>
-                </div>
-                <div className='font-semibold text-1xl'>
-                  <h4> {totalPrice} /-</h4>
-                </div>
-            </div>
+//                    <button 
+//                    className='p-1 bg-teal-300 text-white font-semibold rounded-md'
+//                     onClick={handleDecreaseItem}
+//                    >
+//                     <FaMinus /></button>
+//                 </div>
+//                 <div className='font-semibold text-1xl'>
+//                   <h4> {totalPrice} /-</h4>
+//                 </div>
+//             </div>
     
-    )
-  }
+//     )
+//   }
 
+const CartItem = ({ data }) => {
+   const dispatch = useDispatch();
+   const totalPrice = Math.floor((data.cartQuantity * (data.defaultPrice || data.price)) / 100);
+ 
+   const handleRemoveItem = () => {
+     dispatch(removeItem(data.id));
+   };
+ 
+   const handleAddItem = () => {
+     dispatch(addItem(data));
+   };
+ 
+   const handleDecreaseItem = () => {
+     dispatch(decrementItem(data));
+   };
+ 
+   return (
+<div className="grid grid-cols-1 md:grid-cols-5 gap-4 items-center border-b-2 border-gray-300 pb-4 md:pb-2">
+  <div className="col-span-2 flex items-center gap-4">
+    <img
+      src={"https://media-assets.swiggy.com/swiggy/image/upload/" + data.imageId}
+      alt={data.name}
+      className="w-20 h-20 object-cover md:w-24 md:h-24"
+    />
+    <div className="flex-1">
+      <h3 className="font-semibold text-sm md:text-base">{data.name}</h3>
+      <button
+        onClick={handleRemoveItem}
+        className="mt-2 text-red-500 hover:text-red-700"
+      >
+        <FaTrash />
+      </button>
+    </div>
+  </div>
+
+  <div className="text-center font-semibold text-lg md:text-base">
+    <h3 className="md:hidden inline-block text-purple-400 md:text-black mr-2 md:mr-0">Price: </h3>
+    <h3 className="inline">{(data.defaultPrice || data.price) / 100}/-</h3>
+  </div>
+
+  <div className="flex items-center justify-center gap-2 md:gap-3">
+    <button
+      className="p-1 bg-teal-300 text-white font-semibold rounded-md"
+      onClick={handleAddItem}
+    >
+      <FaPlus />
+    </button>
+    <h3 className="font-semibold text-lg md:text-base">
+      <span className="md:hidden text-purple-400 md:text-black mr-2 md:mr-0">Quantity: </span>{data.cartQuantity}
+    </h3>
+    <button
+      className="p-1 bg-teal-300 text-white font-semibold rounded-md"
+      onClick={handleDecreaseItem}
+    >
+      <FaMinus />
+    </button>
+  </div>
+
+  <div className="text-center font-semibold text-lg md:text-base ">
+    <h3 className="md:hidden inline-block text-purple-400 md:text-black mr-2 md:mr-0">Total Price: </h3>
+    <h4 className="inline">{totalPrice}/-</h4>
+  </div>
+</div>
+
+   );
+ };
 export default CartItem
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,29 +3,58 @@ import CARTIMG from '../assets/cart.webp';
 import CartItem from '../Components/CartItem';
 import { useSelector } from 'react-redux';
 
+// const Cart = () => {
+//   const cartItems = useSelector((state) => state.cart.items);
+
+//   return (
+//     <div className='w-11/12 p-4 mx-auto'>
+//       {cartItems.length > 0 ? (
+//         <div className='mt-10'>
+//           <div className='w-full flex justify-between h-full border-b-[5px] border-teal-300 my-2'>
+//             <h3 className='text-[15px] md:text-2xl text-center'>Product</h3>
+//             <h3 className='text-[15px] md:text-2xl text-center'>Price</h3>
+//             <h3 className='text-[15px] md:text-2xl text-center'>Quantity</h3>
+//             <h3 className='text-[15px] md:text-2xl text-center'>Total</h3>
+//           </div>
+//           <div className='cart-items mt-4'>
+//             {cartItems.map((item) => (
+//               <CartItem key={item.id} data={item} />
+//             ))}
+//           </div>
+//         </div>
+//       ) : (
+//         <div className='w-full text-center flex flex-col items-center justify-center md:h-[512px] h-[550px]'>
+//           <h1 className='my-2 font-semibold text-2xl text-teal-500'>No items in the cart, shop now!</h1>
+//           <img src={CARTIMG} alt="Empty Cart" className='mb-2 w-[300px] object-cover' />
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
 const Cart = () => {
   const cartItems = useSelector((state) => state.cart.items);
 
   return (
-    <div className='w-11/12 p-4 mx-auto'>
+    <div className="w-11/12 p-4 mx-auto">
       {cartItems.length > 0 ? (
-        <div className='mt-10'>
-          <div className='w-full flex justify-between h-full border-b-[5px] border-teal-300 my-2'>
-            <h3 className='text-[15px] md:text-2xl text-center'>Product</h3>
-            <h3 className='text-[15px] md:text-2xl text-center'>Price</h3>
-            <h3 className='text-[15px] md:text-2xl text-center'>Quantity</h3>
-            <h3 className='text-[15px] md:text-2xl text-center'>Total</h3>
+        <div className="mt-10">
+          <div className="hidden md:grid grid-cols-5 gap-4 text-center font-semibold text-lg border-b-2 border-teal-300 pb-2">
+            <h3 className="col-span-2">Product</h3>
+            <h3>Price</h3>
+            <h3>Quantity</h3>
+            <h3>Total</h3>
           </div>
-          <div className='cart-items mt-4'>
+          <div className="cart-items mt-4 space-y-6">
             {cartItems.map((item) => (
               <CartItem key={item.id} data={item} />
             ))}
           </div>
         </div>
       ) : (
-        <div className='w-full text-center flex flex-col items-center justify-center md:h-[512px] h-[550px]'>
-          <h1 className='my-2 font-semibold text-2xl text-teal-500'>No items in the cart, shop now!</h1>
-          <img src={CARTIMG} alt="Empty Cart" className='mb-2 w-[300px] object-cover' />
+        <div className="w-full text-center flex flex-col items-center justify-center md:h-[512px] h-[550px]">
+          <h1 className="my-2 font-semibold text-2xl text-teal-500">No items in the cart, shop now!</h1>
+          <img src={CARTIMG} alt="Empty Cart" className="mb-2 w-[300px] object-cover" />
         </div>
       )}
     </div>
